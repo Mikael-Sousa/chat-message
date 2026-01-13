172b5a2017b03374471d517e974442211761a5e9
Mikael-Sousa <mikael.sr.dev@gmail.com>
2026-01-13 15:48:54 -0300

    feat(auth): adiciona criptografia de senha na rota de registro

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -1,18 +1,26 @@
 const userModel = require("../models/user.model");
+const bcrypt = require("bcrypt");
 import type { User } from '../types/index';
 
 const registerUser = async (user: User) => {
-  const existingUser = await userModel.findByEmail(user.email);
+    const existingUser = await userModel.findByEmail(user.email);
 
-  if (existingUser) {
-    throw new Error("EMAIL_ALREADY_EXISTS");
-  }
+    if (existingUser) {
+        throw new Error("EMAIL_ALREADY_EXISTS");
+    }
 
-  const newUser = await userModel.registerNewUser(user);
+    const hashedPassword = await bcrypt.hash(user.password, 10)
 
-  return newUser;
+    const newUser = await userModel.registerNewUser({
+        username: user.username,
+        email: user.email,
+        password: hashedPassword
+    });
+
+
+    return newUser;
 };
 
 module.exports = {
-  registerUser,
+    registerUser,
 };
