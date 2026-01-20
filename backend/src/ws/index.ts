18ee3252431a640f523b6936cfda991b7cd218df
Mikael-Sousa <mikael.sr.dev@gmail.com>
2026-01-20 14:19:23 -0300

    feat: chat wss 1:1

--- backend/src/ws/index.ts
+++ backend/src/ws/index.ts
@@ -1,24 +1,68 @@
+const jwt = require("jsonwebtoken");
+
 function setupWS(wss: any) {
-    wss.on("connection", () => {
+    const clients = new Map();
+
+    wss.on("connection", (ws: any) => {
         console.log("WS conectado");
 
-        wss.on("message", (data: JSON) => {
-            const message = data.toString();
-            console.log("📩 Mensagem recebida:", message);
+        ws.isAuthenticated = false;
 
-            wss.send(
-                JSON.stringify({
-                    type: "MESSAGE_RECEIVED",
-                    payload: message,
-                })
-            );
+        ws.on("message", (data: any) => {
+            let msg;
+
+            try {
+                msg = JSON.parse(data.toString());
+            } catch {
+                ws.close();
+                return;
+            }
+
+            if (msg.type === "AUTH") {
+                try {
+                    const decoded = jwt.verify(msg.token, process.env.JWT_SECRET!);
+
+                    ws.userId = decoded.id;
+                    ws.isAuthenticated = true;
+
+                    clients.set(ws.userId, ws);
+
+                    ws.send(JSON.stringify({
+                        type: "AUTH_SUCCESS"
+                    }));
+
+                    console.log("Autenticado:", ws.userId);
+                } catch {
+                    ws.close();
+                }
+                return;
+            }
+
+            if (!ws.isAuthenticated) {
+                ws.close();
+                return;
+            }
+
+            if (msg.type === "PRIVATE_MESSAGE") {
+                const receiver = clients.get(msg.to);
+
+                if (receiver) {
+                    receiver.send(JSON.stringify({
+                        type: "PRIVATE_MESSAGE",
+                        from: ws.userId,
+                        message: msg.message
+                    }));
+                }
+            }
         });
 
-        wss.on("close", () => {
-            console.log("WS desconectado");
+        ws.on("close", () => {
+            if (ws.userId) {
+                clients.delete(ws.userId);
+                console.log("Desconectou:", ws.userId);
+            }
         });
-    }
-    )
+    });
 }
 
-module.exports = setupWS
+module.exports = setupWS;
